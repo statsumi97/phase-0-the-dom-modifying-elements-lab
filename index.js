1569e95ad044551367abcb1bc03cd755c2d2a953
Statz Tatsumi Saines <statsumi.sts@gmail.com>
2023-10-23 14:27:21 -0400

    final product

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Statz is the champion';
+mainElement.remove();
+newHeader.id = 'victory';
+newHeader.textContent = 'Statz is the champion'; // Replace 'YOUR-NAME' with your name
